2f5c0ca6b32d8e8e37c373ad1c677fa844e04495
busyxin <xin@busyxin.com>
2018-10-19 18:21:45 +0200

    Fix classifier

--- movie_classifier.py
+++ movie_classifier.py
@@ -37,18 +37,18 @@ with open('data/train/movie_lines.txt', 'r', errors='ignore') as f:
   lines = f.readlines()
 
 for line in lines:
-  l = list(map(lambda x: x.strip(), line.split("+++$+++")))
+  l = list(map(lambda x: x.strip(), line.lower().split("+++$+++")))
   d = {
     "lineID": l[0],
     "characterID": l[1],
     "movieID": l[2],
     "characterName": l[3],
-    "line": l[4].lower()
+    "line": l[4]
   }
 
   movie_lines.append(d)
 
-  movies[l[2]]["lines"].append(l[4].lower())
+  movies[l[2]]["lines"].append(l[4])
 
 
   # print (movies[l[2]]["genre"])
@@ -56,17 +56,47 @@ for line in lines:
 
   movies[l[2]]["target"] = categories.index(movies[l[2]]["genre"][0])
 
-
 train = {
   "data": [],
   "target": []
 }
 
 for key in movies:
-  train["data"].append(movies[key]["lines"])
-  train["target"].append(movies[key]["target"])
 
-print (train)
+  train["data"].append(' '.join(movies[key]["lines"]))
+
+  train["target"].append(movies[key]["target"])
+  if key == "m108":
+    print (train["target"])
+
+
+count_vect = CountVectorizer()
+
+X_train_counts = count_vect.fit_transform(train["data"])
+
+X_train_counts.shape
+tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
+X_train_tf.shape
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train_tfidf.shape
+
+clf = MultinomialNB().fit(X_train_tfidf, train["target"])
+
+docs_new = ['love you darling', 'hell', 'kill', 'fbi', 'crap', 'darling', 'honey', "happy", "I'm just glad we're still together, Lisa, because I need you this yearen"]
+X_new_counts = count_vect.transform(docs_new)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+for doc, cat in zip(docs_new, predicted):
+  print ("The sentence:", doc)
+  print ("Belongs to the genre:", categories[cat])
+  print ()
+  # print('%r => %s' % (doc, twenty_train.target_names[category]))
+
 
 
 
@@ -94,7 +124,7 @@ for line in lines:
 ######### LOAD CHARACTERS LINES #########
 tokenizer = RegexpTokenizer(r'\w+')
 
-count_vect = CountVectorizer()
+# count_vect = CountVectorizer()
 
 for ml in movie_lines:
   characters[ml["characterID"]]["lines"].append(ml["line"])
@@ -156,8 +186,8 @@ def getMovieGenres():
       else:
         genres[genre] = 1
 
-  for genre in genres:
-    print (genre, genres[genre])
+  # for genre in genres:
+  #   print (genre, genres[genre])
 
 getMovieGenres()
 
